c22125956ceccad63ece62435fe94de4d6387444
MrXu <mrxu1010@outlook.com>
2017-05-03 23:22:19 +0800

    package

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -33,7 +33,7 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style scoped lang = 'less'>
 h1,
 h2 {
   font-weight: normal;
@@ -42,11 +42,13 @@ h2 {
 ul {
   list-style-type: none;
   padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
+  li {
+    display: inline-block;
+    margin: 0 10px;
+    a{
+      color: red;
+    }
+  }
 }
 
 a {
